bb8263b011b46409c4017c19893e8401400295d1
Kartik Pandey <kartikpandeyindia@gmail.com>
2023-01-22 10:37:32 +0530

    Prob 6

---
+++ 06.cpp
@@ -0,0 +1,25 @@
+//Find the difference of "sum of squares" and "square of sums" of first 100 natural numbers
+
+#include <iostream>
+#define data unsigned long long
+
+data func(int k)
+{
+    return ( k * (k + 1) * (k - 1) * (3 * k + 2) ) / 12;
+}
+
+int main ()
+{
+    std::cout << func(100);
+    return 0;
+}
+
+/*
+* Square of Sums (SqS) = ( Ei )^2  //where E is summation notation  
+  Sum of Squares (SSq) = E(i)^2
+  Diff = SqS - SSq
+  --Mathematically--
+  SqS = [ k(k+1)/2 ]^2
+  SSq = [ k(k+1)(2k+1) ]/6
+  for first k natural numbers
+*/
